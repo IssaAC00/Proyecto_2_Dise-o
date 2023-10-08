d45b977414c3d5b5316366d229b3f77a13507686
Rebbox <37891786+RemmosLost@users.noreply.github.com>
2023-10-07 21:29:58 -0600

    Puzzle Kitchen
    Puzzle completado, faltan botones

--- Proyecto2/Assets/Scripts/Kitchen/GameManager1.cs
+++ Proyecto2/Assets/Scripts/Kitchen/GameManager1.cs
@@ -4,6 +4,7 @@ using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class GameManager1 : MonoBehaviour
 {
@@ -11,9 +12,14 @@ public class GameManager1 : MonoBehaviour
     private Sprite bgImage;
     public Sprite[] puzzles;
     public List<Sprite> gamePuzzles = new List<Sprite>();
-
     public List<Button> btns = new List<Button>();
-
+    private bool firstGuess, secondGuess;
+    private int countGuess;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+    private int firstGuessIndex, secondGuessIndex;
+    private string firstGuessesPuzzle, secondGuessesPuzzle;
+    public GameObject GameWinPopUp;
     private void Awake()
     {
         puzzles = Resources.LoadAll<Sprite>("PuzzleImagenes/animals-6392140_1280");
@@ -24,6 +30,8 @@ public class GameManager1 : MonoBehaviour
         GetButtons();
         AddListeners();
         AddGamePuzzles();
+        gameGuesses = gamePuzzles.Count / 2;
+        Shuffle(gamePuzzles);
     }
 
    void GetButtons()
@@ -42,14 +50,14 @@ public class GameManager1 : MonoBehaviour
     {
         int looper = btns.Count;
         int index = 0;
-        for(int i =0; i < looper; i++)
+        for(int i = 0; i < looper; i++)
         {
             if(index == looper / 2)
             {
                 index = 0;
-                gamePuzzles.Add(puzzles[index]);
-                index++;
             }
+            gamePuzzles.Add(puzzles[index]);
+            index++;
         }
             
      }
@@ -64,7 +72,80 @@ public class GameManager1 : MonoBehaviour
 
     public void PickPuzzle()
     {
-        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
-        print("Hey" + name);
+        //string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        if (!firstGuess)
+        {
+            firstGuess = true;
+            firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            firstGuessesPuzzle = gamePuzzles[firstGuessIndex].name;
+            btns[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+        }else if (!secondGuess)
+        {
+            secondGuess = true;
+            secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            secondGuessesPuzzle = gamePuzzles[secondGuessIndex].name;
+            btns[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+
+            if(firstGuessesPuzzle == secondGuessesPuzzle)
+            {
+                print("Puzzle Match");
+            }
+            else
+            {
+                print("Puzzle don't Match");
+            }
+            StartCoroutine(checkThePuzzleMatch());
+        }
+    }
+    IEnumerator checkThePuzzleMatch()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (firstGuessesPuzzle == secondGuessesPuzzle)
+        {
+            yield return new WaitForSeconds(0.5f);
+            btns[firstGuessIndex].interactable = false;
+            btns[secondGuessIndex].interactable = false;
+            btns[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            btns[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+            CheckTheGameFinished();
+
+        }
+        else
+        {
+            btns[firstGuessIndex].image.sprite = bgImage;
+            btns[secondGuessIndex].image.sprite = bgImage;
+        }
+        yield return new WaitForSeconds(0.5f);
+        firstGuess = secondGuess = false;
+    }
+    void CheckTheGameFinished()
+    {
+        countCorrectGuesses++;
+        if(countCorrectGuesses == gameGuesses)
+        {
+            print("Game Finish");
+            GameWinPopUp.SetActive(true);
+            print("it took you " + countGuess + "");
+        }
+    }
+    public void GotoBtnClick()
+    {
+        print("next click");
+    }
+
+    public void RetryBtnClick()
+    {
+        print("retry click");
+    }
+
+    void Shuffle(List<Sprite> list)
+    {
+        for(int i = 0; i<list.Count; i++)
+        {
+            Sprite temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
     }
 }
