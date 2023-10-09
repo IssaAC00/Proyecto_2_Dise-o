70ac386128bf29e04c7d4f9f3c04f1e793562d38
Adrián Rivera Ávila <adriveravila64@gmail.com>
2023-10-08 20:09:49 -0600

    Lobby Letter & Sprites

--- Proyecto2/Assets/Scripts/LevelChanger.cs
+++ Proyecto2/Assets/Scripts/LevelChanger.cs
@@ -4,13 +4,6 @@ public class LevelChanger : MonoBehaviour
 {
   [SerializeField]
   private Animator animator;
-  private void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      FadeToLevel();
-    }
-  }
   public void FadeToLevel()
   {
     animator.SetTrigger("FadeOut");
