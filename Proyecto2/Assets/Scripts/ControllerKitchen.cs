e2301f343dcc61df141dc4e1a97d321535bc49d1
Rebbox <37891786+RemmosLost@users.noreply.github.com>
2023-10-10 22:07:20 -0600

    kitchen

---
+++ Proyecto2/Assets/Scripts/ControllerKitchen.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private SceneInfo sceneInfo;
+    [SerializeField]
+    private Canvas canvas;
+    public string Kitchen;
+
+    private void Start()
+    {
+        transform.position = sceneInfo.PlayerKitchenPos;
+        canvas.enabled = false;
+    }
+
+    private void Interaction()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position,1f);
+        foreach (Collider2D collider in colliders)
+        {
+            if(collider.gameObject.name == "Carta")
+            {
+                if(sceneInfo.KitchenPuzzleCompleted == false)
+                {
+                    sceneInfo.PlayerKitchenPos = transform.position;
+                    canvas.enabled = true;
+
+                }
+            }
+        }
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Interaction();
+        }
+    }
+}
